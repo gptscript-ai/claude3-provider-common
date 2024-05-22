52dfb50b690f48144b712ea854f8c89ac42d1341
Taylor Price <tayworm@gmail.com>
2024-05-22 12:18:56 -0700

    test
    Signed-off-by: Taylor Price <tayworm@gmail.com>

--- claudelib/main.py
+++ claudelib/main.py
@@ -15,15 +15,16 @@ def log(*args):
         print(*args)
 
 
-async def list_models() -> JSONResponse:
-    return JSONResponse(content={"data": [
-        {"id": "claude-3-opus-20240229", "name": "Anthropic Claude 3 Opus"},
-        {"id": "claude-3-sonnet-20240229", "name": "Anthropic Claude 3 Sonnet"},
-        {"id": "claude-3-haiku-20240307", "name": "Anthropic Claude 3 Haiku"},
-        {"id": "anthropic.claude-3-opus-20240229-v1:0", "name": "AWS Bedrock Anthropic Claude 3 Opus"},
-        {"id": "anthropic.claude-3-sonnet-20240229-v1:0", "name": "AWS Bedrock Anthropic Claude 3 Sonnet"},
-        {"id": "anthropic.claude-3-haiku-20240307-v1:0", "name": "AWS Bedrock Anthropic Claude 3 Haiku"},
-    ]})
+async def list_models(client: AsyncAnthropic | AsyncAnthropicBedrock) -> JSONResponse:
+    if type(client) == AsyncAnthropic:
+        data = [{"id": "claude-3-opus-20240229", "name": "Anthropic Claude 3 Opus"},
+                {"id": "claude-3-sonnet-20240229", "name": "Anthropic Claude 3 Sonnet"},
+                {"id": "claude-3-haiku-20240307", "name": "Anthropic Claude 3 Haiku"}, ]
+    else:
+        data = [{"id": "anthropic.claude-3-opus-20240229-v1:0", "name": "AWS Bedrock Anthropic Claude 3 Opus"},
+                {"id": "anthropic.claude-3-sonnet-20240229-v1:0", "name": "AWS Bedrock Anthropic Claude 3 Sonnet"},
+                {"id": "anthropic.claude-3-haiku-20240307-v1:0", "name": "AWS Bedrock Anthropic Claude 3 Haiku"}, ]
+    return JSONResponse(content={"data": data})
 
 
 def map_req(req: dict) -> dict:
